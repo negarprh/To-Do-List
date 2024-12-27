07e74aa85da149747fab4d958cfe077b2a2db3cc
negarprh <negarpr@hotmail.com>
2024-12-26 19:10:28 -0500

    Adding requirements

--- app.py
+++ app.py
@@ -1,66 +1,82 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import uuid
+import os
 
-app = Flask(__name__) # Refering the File
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # config sql, telling the app where is the database
-db = SQLAlchemy(app) #inisialise our db
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "default_key")  # Required for sessions
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///test.db")
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    session_id = db.Column(db.String(50), nullable=False)  # Unique for each user
 
     def __repr__(self):
-        return '<Task r%>' % self.id # Everytime making a task, it returned its id
+        return f'<Task {self.id}>'
+
+@app.before_request
+def set_user_session():
+    """Set a unique session ID for each user."""
+    if 'user_id' not in session:
+        session['user_id'] = str(uuid.uuid4())
+
 @app.route('/', methods=['POST', 'GET'])
-def index():  # put application's code here
+def index():
+    """Main route for displaying tasks."""
+    user_id = session['user_id']
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        new_task = Todo(content=task_content, session_id=user_id)
 
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue adding your task!'
+        except Exception as e:
+            return f"There was an issue adding your task: {e}"
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()  # Looking at database contects and return all of them, here is by date from newst to oldest and then return all of them
+        tasks = Todo.query.filter_by(session_id=user_id).order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    """Delete a task."""
+    user_id = session['user_id']
+    task_to_delete = Todo.query.filter_by(id=id, session_id=user_id).first_or_404()
 
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
-    except:
-        return 'There was an issue deleting your task!'
-
+    except Exception as e:
+        return f"There was an issue deleting your task: {e}"
 
 @app.route('/update/<int:id>', methods=['POST', 'GET'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    """Update a task."""
+    user_id = session['user_id']
+    task = Todo.query.filter_by(id=id, session_id=user_id).first_or_404()
 
     if request.method == 'POST':
         task.content = request.form['content']
+        task.date_created = datetime.strptime(request.form['date_created'], '%Y-%m-%d')
+
 
         try:
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue updating your task!'
+        except Exception as e:
+            return f"There was an issue updating your task: {e}"
     else:
         return render_template('update.html', task=task)
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
